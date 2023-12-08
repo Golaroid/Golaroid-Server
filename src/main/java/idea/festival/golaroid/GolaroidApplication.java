13da729213201fa27562c36e90b7fad498af0845
JuuuuHong <juhang720@gmail.com>
2023-12-08 14:06:02 +0900

    initiall commit

---
+++ src/main/java/idea/festival/golaroid/GolaroidApplication.java
@@ -0,0 +1,13 @@
+package idea.festival.golaroid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolaroidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolaroidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idea/festival/golaroid/GolaroidApplicationTests.java
@@ -0,0 +1,13 @@
+package idea.festival.golaroid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolaroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
