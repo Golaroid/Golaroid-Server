7b6d243e59c02ffcbe380e1ab6f220e4045dfd4d
JuuuuHong <juhang720@gmail.com>
2023-12-09 17:25:21 +0900

    add :: error code

---
+++ src/main/java/gsm/festival/golaroid/GolaroidApplication.java
@@ -0,0 +1,13 @@
+package gsm.festival.golaroid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolaroidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolaroidApplication.class, args);
+	}
+
+}
