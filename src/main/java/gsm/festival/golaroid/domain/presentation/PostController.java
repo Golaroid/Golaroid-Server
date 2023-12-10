496e8c5daa8c7c6baeb5893f44bd094d9abd1244
JuuuuHong <juhang720@gmail.com>
2023-12-11 00:47:17 +0900

    add :: query post by id api

--- src/main/java/gsm/festival/golaroid/domain/presentation/PostController.java
+++ src/main/java/gsm/festival/golaroid/domain/presentation/PostController.java
@@ -1,10 +1,12 @@
 package gsm.festival.golaroid.domain.presentation;
 
 import gsm.festival.golaroid.domain.post.service.PostService;
+import gsm.festival.golaroid.domain.presentation.dto.response.QueryPostDetailsResponse;
 import gsm.festival.golaroid.domain.presentation.dto.response.QueryPostsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -19,6 +21,12 @@ public class PostController {
     @GetMapping
     public ResponseEntity<List<QueryPostsResponse>> queryPosts() {
         List<QueryPostsResponse> response = postService.queryPostsService();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{post_id}")
+    public ResponseEntity<QueryPostDetailsResponse> queryPostById(@PathVariable("post_id") Long postId) {
+        QueryPostDetailsResponse response = postService.queryPostDetails(postId);
         return ResponseEntity.ok(response);
     }
 }
