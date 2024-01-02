9ff87290f05072ea6496e9718a5dceae3c986da0
JuuuuHong <juhang720@gmail.com>
2024-01-02 10:06:30 +0900

    add :: health check

---
+++ src/main/java/gsm/festival/golaroid/global/health/HealthController.java
@@ -0,0 +1,12 @@
+package gsm.festival.golaroid.global.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
